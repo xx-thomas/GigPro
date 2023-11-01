77cfc8c39d93bc3575e00a02f7d8123fb48addd2
thomas <thomas.varghese@columbia.edu>
2023-11-01 06:07:32 -0400

    add create via form

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,9 +1,22 @@
 class GigsController < ApplicationController
   def index
-	@gigs = Gig.all
+		@gigs = Gig.all
   end
 
   def show
-	@gig = Gig.find(params[:id])
+		@gig = Gig.find(params[:id])
   end
+
+  def new
+		@gig = Gig.new # Used for the form
+	end
+
+	def create
+		@gig = Gig.new(title:"...", description:"...")
+		if @gig.save
+      redirect_to @gig
+    else
+      render :new, status: :unprocessable_entity # On failure goes back to New aka beginning with status unprocessable
+    end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/gigs", to: "gigs#index"
-  get "/gigs/:id", to: "gigs#show"
-
+	#   get "/gigs", to: "gigs#index"
+	#   get "/gigs/:id", to: "gigs#show"
+	resources :gigs
   # Defines the root path route ("/")
   # root "posts#index"
 end

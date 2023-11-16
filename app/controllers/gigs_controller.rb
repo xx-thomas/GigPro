0f621a4ac1e165c29026ec4442bca7da69b19ca9
thomas <thomas.varghese@columbia.edu>
2023-11-15 20:08:52 -0500

    require login to touch a gig

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,4 +1,5 @@
 class GigsController < ApplicationController
+	before_action :logged_in_user, only:[:new, :edit, :destroy, :complete]
   def index
     @gigs = Gig.all
   end
@@ -49,6 +50,13 @@ class GigsController < ApplicationController
 		@gig.destroy
 		flash[:success] = "Gig: '#{@gig.title}' was completed!."
 		redirect_to action: "index"
+	end
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to login_url, status: :see_other
+		end
 	end
 
   private

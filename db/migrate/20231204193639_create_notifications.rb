3e8c4fcb1a0beed4587504356e3005f88fcd3ca1
thomas <thomas.varghese@columbia.edu>
2023-12-04 16:43:44 -0500

    add type to notif

--- db/migrate/20231204193639_create_notifications.rb
+++ db/migrate/20231204193639_create_notifications.rb
@@ -2,6 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[7.1]
   def change
     create_table :notifications do |t|
       t.boolean :read
+			t.string  :type
       t.references :user, null: false, foreign_key: true
       t.references :gig, null: false, foreign_key: true
 

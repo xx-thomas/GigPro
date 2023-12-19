cf33c90165125359865a6b68dea4c35ffbd15dee
Stacey Yao <sy3042@columbia.edu>
2023-12-18 20:57:32 -0500

    latest

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :recipient, class_name: 'User', foreign_key: 'recipient_id'
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  #belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :content, presence: true
   validates :rating_value, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
